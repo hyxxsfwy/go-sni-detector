872a82dbeb8a38a7749daf33c7d3685ae16db906
wenchao <WenChao.IT@outlook.com>
2022-07-14 11:23:35 +0800

    打包编译命令

---
+++ build_cmd.bat
@@ -0,0 +1,11 @@
+:: Windows 主机编译 Windows 客户端
+SET CGO_ENABLED=0
+SET GOOS=windows
+SET GOARCH=amd64
+go build -o .\build\go-sni-detector_windows_amd64.exe
+
+::Windows 主机编译 LINUX 客户端
+SET CGO_ENABLED=0
+SET GOOS=linux
+SET GOARCH=amd64
+go build -o .\build\go-sni-detector_linux_amd64.bin
